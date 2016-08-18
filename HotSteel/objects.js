e1ab15010d86282bb9d34614fff46cae95f6eea5
JasperCherry <kacper.wisnicki@gmail.com>
2016-08-18 05:08:14 +0100

    graphics in progress
    x

--- HotSteel/objects.js
+++ HotSteel/objects.js
@@ -1,4 +1,71 @@
-function DeadBody(x, y, angle1, angle2) {
+function DeadBodyFire(x,y){
+
+    this.phase=1;
+    this.phaseTimer=1000;
+    this.fireLoop=0;
+    this.delayTime=0;
+    this.x=x;
+    this.y=y;
+
+    // drawing the fire
+    this.update = function() {
+      ctx = myGameArea.context;
+      ctx.save();
+      ctx.translate(this.x-64, this.y-110);
+
+      console.log(this.phaseTimer);
+
+      if(this.phase==1){
+        ctx.drawImage(document.getElementById("s"+this.fireLoop), 0, 0);
+        this.delayTime++;
+        if(this.delayTime==3){
+          this.delayTime=0;
+          this.fireLoop++;
+          if(this.fireLoop==8){
+          this.fireLoop=0;
+          this.phase=2;
+          }
+        }
+      }else if(this.phase==2){
+        ctx.drawImage(document.getElementById("l"+this.fireLoop), 0, 0);
+        this.delayTime++;
+        if(this.delayTime==3){
+          this.delayTime=0;
+          this.fireLoop++;
+          this.phaseTimer--;
+          if(this.fireLoop==10){
+          this.fireLoop=0;
+          }
+          if(this.phaseTimer==0){
+            this.phase=3;
+          }
+        }
+      }else if(this.phase==3){
+        ctx.drawImage(document.getElementById("e"+this.fireLoop), 0, 0);
+        this.delayTime++;
+        if(this.delayTime==3){
+          this.delayTime=0;
+          this.fireLoop++;
+          if(this.fireLoop==6){
+          this.fireLoop=0;
+          this.phase=4;
+          }
+        }
+      }else if(this.phase==4){
+
+      }
+
+      ctx.restore();
+    }
+
+}
+
+
+function DeadBody(x, y, angle1, angle2, img1, img2) {
+
+
+    this.img1=img1;
+    this.img2=img2;
 
     this.widthBody = 30;
     this.heightBody = 50;
@@ -6,32 +73,42 @@ function DeadBody(x, y, angle1, angle2) {
     this.widthTower = 3;
 
     this.color = "black";
-
     this.angleBody = angle1;
     this.angleTower = angle2;
-
     this.x = x;
     this.y = y;
+
     this.update = function() {
         // drawing the body
         ctx = myGameArea.context;
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angleBody);
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.widthBody / -2, this.heightBody / -2, this.widthBody, this.heightBody);
+        ctx.drawImage(this.img1, -20, -30);
+        //ctx.fillStyle = this.color;
+        //ctx.fillRect(this.widthBody / -2, this.heightBody / -2, this.widthBody, this.heightBody);
         ctx.restore();
 
-        // drawing the barrel
+        // drawing the tower
         ctx = myGameArea.context;
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angleTower);
-        ctx.translate(0, -20);
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.widthTower / -2, this.heightTower / -2, this.widthTower, this.heightTower);
+        // tower of grey tank should be in different position
+        if(this.img2==gerbw){
+          ctx.translate(0, -3);
+          ctx.drawImage(this.img2, -20, -30);
+        }else{
+          ctx.drawImage(this.img2, -20, -30);
+        }
+
+        //ctx.translate(0, -20);
+        //ctx.fillStyle = this.color;
+        //ctx.fillRect(this.widthTower / -2, this.heightTower / -2, this.widthTower, this.heightTower);
         ctx.restore();
-        // drawing the tower
+
+        // drawing the tower round part
+        /*
         ctx = myGameArea.context;
         ctx.save();
         ctx.translate(this.x, this.y);
@@ -41,6 +118,7 @@ function DeadBody(x, y, angle1, angle2) {
         ctx.closePath();
         ctx.fill();
         ctx.restore();
+        */
     }
 }
 
@@ -48,7 +126,7 @@ function DeadBody(x, y, angle1, angle2) {
 function Body(color, x, y, image) {
 
 
-    this.img=document.getElementById(image);
+    this.img=image;
 
     this.numMines = 10;
     this.numBullet = 20;
@@ -96,7 +174,7 @@ function Body(color, x, y, image) {
 
 function Tower(radius, width, height, color, x, y, image) {
 
-    this.img=document.getElementById(image);
+    this.img=image;
 
     this.radius = radius;
     this.width = width;
@@ -125,7 +203,14 @@ function Tower(radius, width, height, color, x, y, image) {
         ctx.fillStyle = color;
         ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
         */
-        ctx.drawImage(this.img, -20, -30);
+
+        // tower in a grey tank should be in different position
+        if(this.img==gerb){
+          ctx.translate(0, -3);
+          ctx.drawImage(this.img, -20, -30);
+        }else{
+          ctx.drawImage(this.img, -20, -30);
+        }
 
         ctx.restore();
 
